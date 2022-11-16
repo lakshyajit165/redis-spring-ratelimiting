7088fd26f168199d61612e7a6adf39fc8d289294
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2022-11-16 21:17:34 +0530

    added integration test cases

--- src/test/java/com/example/redisspringratelimiting/RedisspringratelimitingApplicationTests.java
+++ src/test/java/com/example/redisspringratelimiting/RedisspringratelimitingApplicationTests.java
@@ -2,32 +2,19 @@ package com.example.redisspringratelimiting;
 
 import com.example.redisspringratelimiting.config.RateLimitConfig;
 import com.example.redisspringratelimiting.config.RedisConfig;
-import com.example.redisspringratelimiting.controller.RateLimitController;
-import com.giffing.bucket4j.spring.boot.starter.config.cache.SyncCacheResolver;
-import com.giffing.bucket4j.spring.boot.starter.config.cache.jcache.JCacheCacheResolver;
-import io.github.bucket4j.distributed.proxy.ProxyManager;
-import io.github.bucket4j.grid.jcache.JCacheProxyManager;
+
+import com.example.redisspringratelimiting.model.ApiResponse;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.redisson.config.Config;
-import org.redisson.jcache.configuration.RedissonConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.cache.CacheManager;
 import javax.cache.Caching;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,29 +22,50 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class RedisspringratelimitingApplicationTests {
 
+    /**
+     * Below mock beans are needed for initializing application context
+     * */
     @MockBean
     RateLimitConfig rateLimitConfig;
 
     @MockBean
     RedisConfig redisConfig;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-
-    @MockBean
-    RateLimitController rateLimitController;
+    /**
+     * Below tests work when the application is already running.
+     * */
 
     @Test
-    public void testAPI() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
-                .get("/user/1")
-                .contentType(MediaType.APPLICATION_JSON)).andReturn();
-        assertEquals(result.getResponse().getStatus(), 200);
+    public void testAPIWithoutRateLimit() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
 
-//        assertEquals(result., "Hello 1");
+        final String baseUrl = "http://localhost:" + 8085 + "/user/1";
+        URI uri = new URI(baseUrl);
+        ResponseEntity<?> result = restTemplate.getForEntity(uri, String.class);
+        // verify response status
+        assertEquals(200, result.getStatusCodeValue());
+        // verify response body
+        assertEquals("{\"message\":\"Success for user 1\"}", result.getBody());
+    }
+
+    @Test
+    public void testAPIWithRateLimitExceeding() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        final String baseUrl = "http://localhost:" + 8085 + "/user/2";
+        URI uri = new URI(baseUrl);
+        for(int i = 0; i < 20; i++) {
+            restTemplate.getForEntity(uri, String.class);
+        }
+        try {
+            ResponseEntity<?> result = restTemplate.getForEntity(uri, String.class);
+        } catch(HttpClientErrorException ex) {
+            // verify response status
+            assertEquals(429, ex.getRawStatusCode());
+            // verify response body
+            assertEquals("{\"message\":\"Rate limit exceeded for user 2\"}", ex.getResponseBodyAsString());
+        }
     }
 }
